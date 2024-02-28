6c708019eabc963efb1b2c5d1979c3f6c54d2bfb
John Grudzinski <johngrudzinski@yahoo.com>
2024-02-28 15:45:26 -0500

    Commit 4

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-const input = document.getElementById('text-input');
 const checkBtn = document.getElementById('check-btn');
 const result = document.getElementById('result');
 const resultWord = document.querySelector('.result-word');
 const resultTxt = document.querySelector('.result-text');
+const input = document.getElementById('text-input').value;
 
-
-function checkPalindrome(input){
-    pattern = /[^a-zA-Z0-9]/g;
-    inputArr = [];
+function checkPalindrome() {
+    const input = document.getElementById('text-input').value;
+    const pattern = /[^a-zA-Z0-9]/g;
+    const inputArr = [];
     for(let i = 0; i < input.length ; i++) {
-        if(input[i] !== patern) {
+        if(!pattern.test(input[i])) {
             inputArr.push(input[i]);
-        }
-    return inputArr;    
+        }  
     }
+    console.log(inputArr);
+    return inputArr;  
 }
-console.log(checkPalindrome());
 
 checkBtn.addEventListener('click', checkPalindrome);
